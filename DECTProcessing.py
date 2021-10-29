de5498742a33473b7c22f0aec5dfb6c09cf84acb
ogdenk <ogdenk@upstate.edu>
2021-10-29 15:58:54 -0400

    Worked on getting 1 row test data running.  Still a problem

--- DECTProcessing.py
+++ DECTProcessing.py
@@ -1,71 +1,70 @@
-# Program to auto-populate matrix of data spreadsheets and identify data with separate key matrix
-
 import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import copy
-
-from sklearn.utils import shuffle
+# import tensorflow as tf
 from keras import models
 from keras import layers
 
-# pathName = "/home/kent/LSpineDECT/"
-pathName = "c:/temp"
+pathName = "/home/kent/LSpineDECT/"
+# pathName = "c:/temp"
 filename = 'Low_Energy_Data.csv'
 
 datafile = os.path.join(pathName, filename)
 pre_data = pd.read_csv(datafile,index_col='Feature_Name')
 
-#listOfFiles = list()
-#listOfPAT = list()
-#filenamexlsx = list()
-#filenamexlsxread = 0
-
 total_rows=len(pre_data.axes[0])
 total_columns=len(pre_data.axes[1])
-# len_group = slices**slices
-number_of_patients = total_rows  # /len_group
+len_group = 1  # slices**slices
+number_of_patients = total_rows/len_group
 valFrac = 1.0 - (number_of_patients - 1.0)/number_of_patients #validation fraction one patient
 trainFrac = 1-valFrac #Training on all but one
 total_Val_Acc = []
 total_Val_Loss = []
 test_patients = []
 for i in range(int(number_of_patients)):
-    data=pre_data   #  Keep original data in pre_data
-    vertebrae = data.index  #  names of vertebral bodies
+    data_df = pre_data   # Keep original data in pre_data
+    vertebrae = data_df.index  # names of vertebral bodies
 
-    testRow = data.iloc[i,:]  # test case i
-    #testRow = testRow.transpose()  #  Doesn't seem to work?
+    testRow_df[1, :] = data_df.iloc[i, :]  # test case i
+    shape = testRow_df.shape
 
-    data = data.drop([data.index[i]])  #  remove the vertebral body being tested
+    data_df = data_df.drop([data_df.index[i]])  # remove the vertebral body being tested
 
-    #test_data_df, train_data_df = np.split(data,[testRow])
+    # test_data_df, train_data_df = np.split(data,[testRow])
+    train_data_df = copy.deepcopy(data_df.iloc[:, :])  # Same as data_df
+    test_data_df[0, :] = copy.deepcopy(testRow_df.iloc[:])  # Same as testRow_df
 
-    train_data_size = len(data)
-    test_data_size = len(testRow)
+    train_data = train_data_df.to_numpy()[:, 1:]  # Convert to numpy arrays, drop BMD values
+    test_data[0, :] = test_data_df.to_numpy()[1:]
 
-    train_labels = copy.deepcopy(data['BMD'].astype('float32'))
-    test_labels = copy.deepcopy(testRow['BMD'].astype('float32'))
+    train_data_size = len(train_data)  # How big are these arrays?
+    test_data_size = len(test_data)
 
-    train_data = copy.deepcopy(data.iloc[:,:])
-    test_data = copy.deepcopy(testRow.iloc[:])
+    train_labels = copy.deepcopy(data_df['BMD'].to_numpy())
+    test_labels = copy.deepcopy(test_data_df[0])
 
     means = train_data.mean(axis=0)
     sigmas = train_data.std(axis=0)
 
-    train_data = (train_data-means)/sigmas
-    test_data = (test_data-means)/sigmas
+    train_data = (train_data - means) / sigmas
+    test_data = (test_data - means) / sigmas
+
+    train_data = np.asarray(train_data)
+    train_labels = np.asarray(train_labels)
+    test_data = np.asarray(test_data)
+    test_labels = np.asarray(test_labels)
 
     model = models.Sequential()
     model.add(layers.Dense(8, activation='relu', input_shape=(train_data.shape[1],)))
     model.add(layers.Dropout(0.5))
-    model.add(layers.Dense(8, activation='relu', input_shape=(train_data.shape[1],)))
+    model.add(layers.Dense(8, activation='relu'))
     #model.add(layers.Dropout(0.5))
     model.add(layers.Dense(1, activation='sigmoid'))
     model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
-    history = model.fit(train_data,train_labels, epochs=20, batch_size=2000, validation_data=(test_data, test_labels))
+    history = model.fit(train_data, train_labels, epochs=20, batch_size=2000, validation_data=(test_data, test_labels))
     history_dict = history.history
     loss_values = history_dict['loss']
     val_loss_values = history_dict['val_loss']
