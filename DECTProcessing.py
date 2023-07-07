668e2e1e32e8d2fe4a540dd9b048494c3020adde
ogdenk <ogdenk@upstate.edu>
2023-07-07 16:56:50 -0400

    Updated version

--- DECTProcessing.py
+++ DECTProcessing.py
@@ -23,19 +23,17 @@ trainFrac = 1-valFrac # Training on all but one
 total_Val_Acc = []
 total_Val_Loss = []
 test_patients = []
-trialsPerPatient = 5
-
 for i in range(int(number_of_patients)):
     data_df = pre_data   # Keep original data in pre_data
     vertebrae = data_df.index  # names of vertebral bodies
 
-        #         Same as df.iloc[[i],:]
-    testRow_df = data_df.iloc[[i]]  # test case i  Use double brackets to get a row back
+    #         Same as df.iloc[[i],:]
+    testRow_df = data_df.iloc[[i]]  # test case i.  Use double brackets to get a row back
     shape = testRow_df.shape
 
     data_df = data_df.drop([data_df.index[i]])  # remove the vertebral body being tested
 
-        # test_data_df, train_data_df = np.split(data,[testRow])
+    # test_data_df, train_data_df = np.split(data,[testRow])
     train_data_df = copy.deepcopy(data_df.iloc[:, :])  # Same as data_df
     test_data_df = copy.deepcopy(testRow_df.iloc[:])  # Same as testRow_df
 
@@ -59,56 +57,59 @@ for i in range(int(number_of_patients)):
     test_data = np.asarray(test_data)
     test_labels = np.asarray(test_labels)
 
-    for j in range(trialsPerPatient):
+    model = models.Sequential()
+    model.add(layers.Dense(8, activation='relu', input_shape=(train_data.shape[1],)))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(8, activation='relu'))
+    # model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1, activation='linear'))
+    model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['mean_squared_error'])
+    history = model.fit(train_data, train_labels, epochs=10, batch_size=5, shuffle=True, validation_split=0.1)  #validation_data=(test_data, test_labels))
 
-        model = models.Sequential()
-        model.add(layers.Dense(8, activation='relu', input_shape=(train_data.shape[1],)))
-        model.add(layers.Dropout(0.5))
-        model.add(layers.Dense(8, activation='relu'))
-        # model.add(layers.Dropout(0.5))
-        model.add(layers.Dense(1, activation='linear'))
-        model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['mean_squared_error'])
+    prediction = model.predict(np.array(test_data))
+    print("Prediction = ", prediction)
+    print("True value = ", test_labels)
 
-        history = model.fit(train_data, train_labels, epochs=5, batch_size=15, validation_data=(test_data, test_labels))
-        history_dict = history.history
-        loss_values = history_dict['loss']
-        val_loss_values = history_dict['val_loss']
-        epochs = range(1, len(loss_values) + 1)
+    #outputs = [layer.output for layer in model.layers]
+    # outputVal = model.layers[len(model.layers)].output
 
+    history_dict = history.history
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    epochs = range(1, len(loss_values) + 1)
 
-        MSE_values = history_dict['mean_squared_error']
-        val_MSE_values = history_dict['val_mean_squared_error']
-        last_val_MSE_value = val_MSE_values[-1]
-        last_val_loss_value = val_loss_values[-1]
-        print(test_data_df.index[0])
-        print(last_val_MSE_value)
-        print(last_val_loss_value)
-        test_patients.append(test_data_df.index[0])
-        total_Val_Acc.append(last_val_MSE_value)
-        total_Val_Loss.append(last_val_loss_value)
+    #val_acc_values = history_dict['val_accuracy']
+    #acc_values = history_dict['accuracy']
+    #last_val_acc_value = val_acc_values[-1]
+    #last_val_loss_value = val_loss_values[-1]
+    #print(test_data_df.index[0])
+    #print(last_val_acc_value)
+   # print(last_val_loss_value)
+    #test_patients.append(test_data_df.index[0])
+   # total_Val_Acc.append(last_val_acc_value)
+   # total_Val_Loss.append(last_val_loss_value)
 
-        #last99Patient_cycle_data = pre_data[total_rows - (total_rows-len_group):]
-        #first1percent_cycle_data = pre_data[:total_rows - (total_rows-len_group)]
-        #pre_data = pd.concat([last99Patient_cycle_data, first1percent_cycle_data])
+    #last99Patient_cycle_data = pre_data[total_rows - (total_rows-len_group):]
+    #first1percent_cycle_data = pre_data[:total_rows - (total_rows-len_group)]
+    #pre_data = pd.concat([last99Patient_cycle_data, first1percent_cycle_data])
+#print(np.mean(total_Val_Acc))
+#print(np.mean(total_Val_Loss))
+#print(total_Val_Acc)
+#print(total_Val_Loss)
+#print(test_patients)
+#plt.plot(epochs, acc_values, 'bo', label='Training acc')
+#plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+#plt.title('Training and validation accuracy')
+#plt.xlabel('Epochs')
+#plt.ylabel('Accuracy')
+#plt.legend()
+#plt.show()
 
-print(np.mean(total_Val_Acc))
-print(np.mean(total_Val_Loss))
-print(total_Val_Acc)
-print(total_Val_Loss)
-print(test_patients)
-plt.plot(epochs, MSE_values, 'bo', label='Training MSE')
-plt.plot(epochs, val_MSE_values, 'b', label='Validation MSE')
-plt.title('Training and validation MSE')
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
-
-plt.clf()
-plt.plot(epochs, loss_values, 'bo', label='Training loss')
-plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
-plt.title('Training and validation loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
+#plt.clf()
+#plt.plot(epochs, loss_values, 'bo', label='Training loss')
+#plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+#plt.title('Training and validation loss')
+#plt.xlabel('Epochs')
+#plt.ylabel('Loss')
+#plt.legend()
+#plt.show()
